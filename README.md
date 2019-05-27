f7d20e88df59133eabf71ab89a2e281d16e41519
Will Stott <will.stott@hotmail.com>
2019-05-27 17:03:53 +0530

    initial files for AwsDotNetCmdUtils v0.0

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # AwsDotNetCmdUtils
 Collection of .NET command line utilities for Amazon Web Services
+
+AwsDotNetS3LargeFileXferCmd
+---------------------------
+Transfers large files to and from S3 storage
---
+++ src/AwsDotNetCmdUtils/AwsDotNetS3LargeFileXferCmd/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AwsDotNetS3LargeFileXferCmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("AwsDotNetS3LargeFileXferCmd v0.0");
+        }
+    }
+}
